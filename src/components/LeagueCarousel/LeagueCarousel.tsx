f92851b27f1e40984f0f9875350b327d09eb435d
becker-finstreet <niklas.becker@finstreet.de>
2025-01-26 17:52:08 +0100

    fix: loader css fix

--- src/components/LeagueCarousel/LeagueCarousel.tsx
+++ src/components/LeagueCarousel/LeagueCarousel.tsx
@@ -40,7 +40,7 @@ export default function LeagueCarousel() {
 
   const { isLoading, error, data } = useLeagues();
 
-  if (isLoading) {
+  if (isLoading || true) {
     return (
       <div className="absolute left-0 right-0 top-0 bottom-0">
         <Loader />;
